444ef513e589dc60a7505cdbd6c34cce6134c5e8
mthsmelo <matheusaguiarmelo@gmail.com>
2022-10-10 23:44:07 -0300

    m

--- 1fundamentos/secao-05-JavaScript:-DOM-eventos-e-webstorage/dia-03-eventos/script.js
+++ 1fundamentos/secao-05-JavaScript:-DOM-eventos-e-webstorage/dia-03-eventos/script.js
@@ -1,130 +1,180 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function createDaysOfTheMonth (){
+ const daysOfTheMonth = document.querySelector('#days') 
+ for (let i =0 ; i<decemberDaysList.length ; i+=1){
+  let createDays = document.createElement('li')
+  createDays.innerHTML=decemberDaysList[i]
+  createDays.className="day"
+
+  if (decemberDaysList[i]=== 4 || decemberDaysList[i]===11 || decemberDaysList[i]===18 || decemberDaysList[i]===25)[
+    createDays.className = "day friday"
+  ]
+  if (decemberDaysList[i]=== 24 || decemberDaysList[i]===31){
+    createDays.className = "day holiday"
   }
-  
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
-  const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  let monthDaysList = document.getElementById("days");
-
-  function createDaysOfTheMonth (){
-    for ( let i = 0; i<decemberDaysList.length ; i+=1){
-        const daysMonth = decemberDaysList[i];
-        const createDays = document.createElement('li')
-        createDays.innerHTML=daysMonth
-
-        if (daysMonth=== 4 || daysMonth === 11 || daysMonth === 18){
-
-            createDays.className = "day friday"
-            monthDaysList.appendChild(createDays)
-        }
-        else if (daysMonth===25){
-
-            createDays.className = "day friday holiday"
-            monthDaysList.appendChild(createDays)
-
-        }
-        else if (daysMonth === 24  || daysMonth === 31) {
-
-            createDays.className = "day holiday"
-            monthDaysList.appendChild(createDays)
-        }
-        else {
-            createDays.className = "day"
-            monthDaysList.appendChild(createDays)
-        }
-    }
+  if (decemberDaysList[i]=== 25){
+    createDays.className = "day friday holiday"
   }
+
+
+  daysOfTheMonth.appendChild(createDays)
+
+ }
+}
 
 createDaysOfTheMonth()
 
 
-function createButton(nomeDoButton) {
-    let ondeButtonEsta = document.getElementsByClassName("buttons-container")[0];
-    let creatingElementButtom = document.createElement('button');
-    creatingElementButtom.id = "btn-holiday";
+function createHolidayButton (string){
 
-    ondeButtonEsta.appendChild(creatingElementButtom);
-    creatingElementButtom.innerHTML = nomeDoButton;
+  const createButton = document.createElement("button")
+  createButton.innerHTML = string
+  createButton.id = "btn-holiday"
+  document.querySelector(".buttons-container").appendChild(createButton)
+}
+
+createHolidayButton("Feriados")
+
+function changeHolidaysBackground (){
+
+document.getElementById("btn-holiday").addEventListener("click", function (){
+
+for (let i = 0; i <document.getElementsByClassName('holiday').length ; i+=1){
+  if (document.getElementsByClassName('holiday')[i].style.backgroundColor === "white"){
+    document.getElementsByClassName('holiday')[i].style.backgroundColor = "rgb(238,238,238)"
   }
-
-  createButton("Feriados") 
-
-  function changeHolidayColor() {
-    let localizeButton = document.querySelector('#btn-holiday');
-    let localizeHolidayClasses = document.querySelectorAll('.holiday');
-   
-  
-    localizeButton.addEventListener('click', function() {
-      for (let i = 0; i < localizeHolidayClasses.length; i += 1) {
-        if (localizeHolidayClasses[i].style.backgroundColor === "white") {
-          localizeHolidayClasses[i].style.backgroundColor = "rgb(238,238,238)";
-        } else {
-          localizeHolidayClasses[i].style.backgroundColor = "white";
-        }
-      }
-    });
-  }
-  changeHolidayColor()
-  
-  function fridayButton (name){
-    let  findButtonSpace = document.querySelector(".buttons-container");
-    let  creatingFridayButton = document.createElement("button");
-
-    creatingFridayButton.innerHTML = name
-    creatingFridayButton.id = "btn-friday"
-    findButtonSpace.appendChild(creatingFridayButton)
+  else {
+    document.getElementsByClassName('holiday')[i].style.backgroundColor = "white"
+  }}
+})
 
 
 
 }
-fridayButton("Sexta-feira");
+changeHolidaysBackground()
+
+function createFridayButton(){
+
+  const createButton = document.createElement("button")
+  createButton.innerHTML = "Sexta-Feira"
+  createButton.id = "btn-friday"
+  document.querySelector(".buttons-container").appendChild(createButton)
+}
 
 
-let fridays = document.querySelectorAll(".friday");
-let botaoFriday = document.querySelector("#btn-friday");
-let decemberFridays = [4 , 11, 18, 25]
+let fridayArray =[4,11,18,25]
+createFridayButton()
 
-function sextouuu (parametro){
+function changeFridayText (){
 
-    botaoFriday.addEventListener("click" , function(){
-        for (let i = 0; i<fridays.length; i+=1){
-            if (fridays[i].innerHTML !== "sexta-feira"){
-            fridays[i].innerHTML = "sexta-feira"}
-            else {
-            fridays[i].innerHTML = parametro[i]    
-            }    
-        }
+  document.getElementById("btn-friday").addEventListener("click", function (){
+  
+  for (let i = 0; i <document.getElementsByClassName('friday').length ; i+=1){
+    if (document.getElementsByClassName('friday')[i].innerHTML !== "Sextou bb"){
+      document.getElementsByClassName('friday')[i].innerHTML = "Sextou bb"
+    }
+    else {
+      document.getElementsByClassName('friday')[i].innerHTML = fridayArray[i]
+    }}
+  }
+)}
+
+changeFridayText()
+
+
+function zoomIn (){
+  for (let i = 0 ; i < document.getElementsByClassName('day').length ; i+=1){
+    document.getElementsByClassName('day')[i].addEventListener('mouseover', function(){
+      document.getElementsByClassName('day')[i].style.fontSize = '40px';
+      document.getElementsByClassName('day')[i].style.fontWeight = '700';
+
+
+
+
+      
     })
-}
-sextouuu(decemberFridays)
 
-let day = document.querySelector('#days');
-function zoomIn(){
 
-    day.addEventListener('mouseover',function(event){
-        event.target.style.fontSize = "50px";
-        event.target.style.fontWeight = '600';
-    });
 
-}
+  }
+  }
 
-function zoomOut(){
- 
-    day.addEventListener('mouseout',function(event){
-    event.target.style.fontSize = "20px"
-    event.target.style.fontWeight = '200'
-} )}
+  zoomIn()
 
-zoomIn();
-zoomOut();
+
+function zoomOut (){
+  for (let i = 0 ; i < document.getElementsByClassName('day').length ; i+=1){
+    document.getElementsByClassName('day')[i].addEventListener('mouseout', function(){
+      document.getElementsByClassName('day')[i].style.fontSize = '20px';
+      document.getElementsByClassName('day')[i].style.fontWeight = '200';
+
+
+
+
+      
+    })
+
+
+
+  }
+  }
+
+  zoomOut()
+
+  function createWork(work){
+    const criaTarefa = document.createElement("span")
+    criaTarefa.innerHTML = work
+
+    document.querySelector(".my-tasks").appendChild(criaTarefa)
+
+  }
+
+
+
+
+  createWork("Fazer os projetos que estão atrasados")
+
+  function createColorWork(cor){
+    const criaCor = document.createElement('div')
+    criaCor.style.backgroundColor=cor
+    document.querySelector('.my-tasks').appendChild(criaCor)
+  }
+
+createColorWork('green')
+
+
+function selectTask (){
+  const selecionarTarefa = document.querySelector('.my-tasks').lastChild
+
+  selecionarTarefa.addEventListener('click', function(){
+    if (selecionarTarefa.className !== "task selected"){
+      selecionarTarefa.className = "task selected"
+    }
+    else {
+      selecionarTarefa.className = "task"
+    }
+    }
+  )
+  }
+
+
+selectTask()
+
+
