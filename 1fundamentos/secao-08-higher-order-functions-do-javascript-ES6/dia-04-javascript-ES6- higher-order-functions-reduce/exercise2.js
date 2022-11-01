b4ca9ba2b262a3cca61138fa0925485dbbb433ec
mthsmelo <matheusaguiarmelo@gmail.com>
2022-11-01 19:35:43 -0300

    exercicios 8.4

---
+++ 1fundamentos/secao-08-higher-order-functions-do-javascript-ES6/dia-04-javascript-ES6- higher-order-functions-reduce/exercise2.js
@@ -0,0 +1,109 @@
+//Considere o seguinte array e manipule-o utilizando reduce para fazer o que é pedido:
+
+const books = [
+    {
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
+    },
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
+    },
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
+    },
+    {
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
+    },
+    {
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
+    },
+];
+
+// 1: Crie uma string com os nomes de todas as pessoas autoras.
+
+// const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames(array) {
+    return array.reduce((acc, curr) => `${acc} ${curr.author.name},`, '')
+}
+
+console.log(reduceNames(books));
+
+// 2: Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+
+//const expectedResult = 43;
+
+function averageAge(array) {
+    return array.reduce((acc, curr) => {
+        return acc + (curr.releaseYear - curr.author.birthYear) / array.length
+    }, 0)
+}
+
+console.log(averageAge(books));
+// 3: Encontre o livro com o maior nome.
+
+
+/* const expectedResult = {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  }; */
+
+function longestNamedBook(array) {
+    return array.reduce((acc, curr) => {
+        if (acc.name.length>curr.name.length){
+            return acc;
+        } else curr;
+    })
+}
+
+console.log(longestNamedBook(books));
---
+++ 1fundamentos/secao-08-higher-order-functions-do-javascript-ES6/dia-04-javascript-ES6- higher-order-functions-reduce/exercise3.js
@@ -0,0 +1,24 @@
+// Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const expectedResult = 20;
+
+function containsA(array) {
+  let contador = 0
+  array.forEach((name) => {
+    contador += name.split('').reduce((acc, curr) => {
+      if (curr === "a" || curr === "A") {
+        return acc + 1
+      } return acc;
+    },0);
+  });
+  return contador;
+}
+
+console.log(containsA(names));
+    // escreva seu código aqui
